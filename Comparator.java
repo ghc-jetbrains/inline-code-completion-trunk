da50dfd6695607cb364abc9349cef533d003ed39
Mason Chen <jiec@microsoft.com>
2024-10-19 14:57:09 +0800

    Update and rename Tests1.java to Comparator.java

---
+++ Comparator.java
@@ -0,0 +1,11 @@
+package com.exmaple;
+
+public class Comparator {
+
+    // Get max number from an int array
+    public int getMax(int n, int m) {
+        return (n > m) ? n : m;
+    }
+
+    //
+}
