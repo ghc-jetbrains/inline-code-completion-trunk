e10b1145343965154bcfd093cf3bdb3dc6a1cafb
Mason Chen <jiec@microsoft.com>
2024-10-19 16:20:25 +0800

    Create Sorter.java

---
+++ package com.exmaple;  import java.util.List;  public class Sorter {     /Sorter.java
@@ -0,0 +1,10 @@
+package com.exmaple;
+
+import java.util.List;
+
+public class Sorter {
+    // write a function to sort list of integers from smallest to largest
+    public static void sort(List<Integer> input) {
+        
+    }
+}
