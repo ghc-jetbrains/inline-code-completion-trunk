07b37c28d0be41edaadff325af8d6c31dffc1a85
Mason Chen <jiec@microsoft.com>
2024-10-19 14:49:51 +0800

    Update Tests1.java

--- Tests1.java
+++ Tests1.java
@@ -2,6 +2,9 @@ package com.exmaple;
 
 public class Tests1 {
 
-    //
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 
+    // Get max from an int array
 }
